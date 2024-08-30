d50b9d313dfb14c11ec0f0130ee0304afdaa30c4
Juan Andres Chaverra De Moya <juan.chaverra@globant.com>
2024-08-30 11:56:55 -0500

    First commit

---
+++ src/test/java/com/globant/test/WdioTest.java
@@ -0,0 +1,13 @@
+package com.globant.test;
+
+import com.globant.utils.test.BaseTest;
+import org.testng.annotations.Test;
+
+public class WdioTest extends BaseTest {
+
+    @Test
+    public void verifyNavigationOnTheTopBottomMenuBar(){
+
+    }
+
+}
